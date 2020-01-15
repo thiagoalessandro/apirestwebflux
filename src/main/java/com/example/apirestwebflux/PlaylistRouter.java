1d009afaf347c905a43daea71769f78348bc30ed
thiago <thiago.alessandro.farias@gmail.com>
2020-01-14 23:39:15 -0300

    aplicação rest assincrona

--- src/main/java/com/example/apirestwebflux/PlaylistRouter.java
+++ src/main/java/com/example/apirestwebflux/PlaylistRouter.java
@@ -8,10 +8,10 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
-@Configuration
+//@Configuration
 public class PlaylistRouter {
 
-    @Bean
+    //@Bean
     public RouterFunction<ServerResponse> route(PlaylistHandler handler){
         return RouterFunctions
                 .route(GET("/playlist").and(accept(MediaType.APPLICATION_JSON)), handler::findAll)
--- src/main/java/com/example/apirestwebflux/controller/PlaylistController.java
+++ src/main/java/com/example/apirestwebflux/controller/PlaylistController.java
@@ -3,12 +3,20 @@ package com.example.apirestwebflux.controller;
 import com.example.apirestwebflux.document.Playlist;
 import com.example.apirestwebflux.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
-//@RequestMapping(value = "/playlist")
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping(value = "/playlist")
 public class PlaylistController {
 
     @Autowired
@@ -27,6 +35,35 @@ public class PlaylistController {
     @PostMapping
     public Mono<Playlist> save(@RequestBody Playlist playlist){
         return playlistService.save(playlist);
+    }
+
+    @GetMapping(value="/webflux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByWebflux(){
+
+        System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> playlistFlux = playlistService.findAll();
+
+        return Flux.zip(interval, playlistFlux);
+
+    }
+
+    @GetMapping(value="/playlist/mvc")
+    public List<String> getPlaylistByMvc() throws InterruptedException {
+
+        System.out.println("---Start get Playlists by MVC--- " + LocalDateTime.now());
+
+
+        List<String> playlistList = new ArrayList<>();
+        playlistList.add("Java 8");
+        playlistList.add("Spring Security");
+        playlistList.add("Github");
+        playlistList.add("Deploy de uma aplicação java no IBM Cloud");
+        playlistList.add("Bean no Spring Framework");
+        TimeUnit.SECONDS.sleep(15);
+
+        return playlistList;
+
     }
 
 }
